e0ae97af8803ff5904ebd3b4b3e7e9e5bc02f548
David Ruitter <David.Ruitter@netgo.de>
2021-06-01 20:34:50 +0200

    Add Command and Request Model

---
+++ src/EasyDoc/EasyDoc.Domain.Core/Commands/Command.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using FluentValidation.Results;
+
+namespace EasyDoc.Domain.Core.Commands
+{
+    public abstract class Command : Request
+    {
+        public DateTimeOffset TimeStamp { get; set; }
+        public ValidationResult ValidationResult { get; set; }
+
+        public Command(Guid aggregateId) : base(aggregateId)
+        {
+            TimeStamp = DateTimeOffset.Now;
+        }
+
+        public abstract bool IsValid();
+    }
+}
---
+++ src/EasyDoc/EasyDoc.Domain.Core/Commands/Request.cs
@@ -0,0 +1,29 @@
+﻿using MediatR;
+using System;
+
+namespace EasyDoc.Domain.Core.Commands
+{
+    public class Request : IRequest
+    {
+        public Guid AggregateId { get; }
+        public string MessageType { get; }
+
+        protected Request(Guid aggregateId)
+        {
+            AggregateId = aggregateId;
+            MessageType = GetType().Name;
+        }
+    }
+
+    public class Request<T> : IRequest<T>
+    {
+        public Guid AggregateId { get; }
+        public string MessageType { get; }
+
+        protected Request(Guid aggregateId)
+        {
+            AggregateId = aggregateId;
+            MessageType = GetType().Name;
+        }
+    }
+}
