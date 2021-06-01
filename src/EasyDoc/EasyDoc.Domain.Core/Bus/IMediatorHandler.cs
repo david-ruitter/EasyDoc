cbee30cf6e40a6a436badc5d6f56474f8f5481f7
David Ruitter <David.Ruitter@netgo.de>
2021-06-01 20:40:46 +0200

    Setup MediatorHandler

---
+++ src/EasyDoc/EasyDoc.Domain.Core/Bus/IMediatorHandler.cs
@@ -0,0 +1,11 @@
+﻿using EasyDoc.Domain.Core.Commands;
+using System.Threading.Tasks;
+
+namespace EasyDoc.Domain.Core.Bus
+{
+    public interface IMediatorHandler
+    {
+        Task SendCommandAsync<T>(T command) where T : Command;
+        Task SendRequestAsync<TResponse>(Request<TResponse> request);
+    }
+}
