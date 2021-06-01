9a50b15ef63fc3b1fa21a6cfbf1f73ef60802299
David Ruitter <David.Ruitter@netgo.de>
2021-06-01 20:40:46 +0200

    Setup MediatorHandler

---
+++ src/EasyDoc/EasyDoc.Domain.Core/Bus/MediatorHandler.cs
@@ -0,0 +1,26 @@
+﻿using EasyDoc.Domain.Core.Commands;
+using MediatR;
+using System.Threading.Tasks;
+
+namespace EasyDoc.Domain.Core.Bus
+{
+    public sealed class MediatorHandler : IMediatorHandler
+    {
+        private readonly IMediator _mediator;
+
+        public MediatorHandler(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        public async Task SendCommandAsync<T>(T command) where T : Command
+        {
+            await _mediator.Send(command);
+        }
+
+        public async Task SendRequestAsync<TResponse>(Request<TResponse> request)
+        {
+            await _mediator.Send(request);
+        }
+    }
+}
