62379d7b0b1c0861273ba0521a57a44ddf294ede
David Ruitter <David.Ruitter@netgo.de>
2021-06-03 10:48:16 +0200

    Add FileInputService

---
+++ src/EasyDoc/EasyDoc.Application/Interfaces/IFileInputService.cs
@@ -0,0 +1,11 @@
+﻿using EasyDoc.Application.Models;
+using System.Collections.Generic;
+
+namespace EasyDoc.Application.Interfaces
+{
+    public interface IFileInputService
+    {
+        List<string[]> GetFilePaths(List<string> folderPaths);
+        List<InputFile> ReadFiles(List<string[]> folders);
+    }
+}
---
+++ src/EasyDoc/EasyDoc.Application/Services/FileInputService.cs
@@ -0,0 +1,71 @@
+﻿using EasyDoc.Application.Interfaces;
+using EasyDoc.Application.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace EasyDoc.Application.Services
+{
+    public class FileInputService : IFileInputService
+    {
+        /// <summary>
+        /// If no filepaths are provided the tool grabs files,
+        /// from the current directory
+        /// </summary>
+        /// <param name="filePaths">Paths to Files that need to be documented</param>
+        /// <returns></returns>
+        public List<string[]> GetFilePaths(List<string> folderPaths)
+        {
+            var folders = new List<string[]>();
+            if (folderPaths.Count == 0)
+            {
+                folders.Add(Directory.GetFiles(Directory.GetCurrentDirectory()));
+            } 
+            else
+            {
+                foreach(var folderPath in folderPaths)
+                {
+                    folders.Add(Directory.GetFiles(@"" + folderPath));
+                }
+            }
+
+            Console.WriteLine("Documents the following Files");
+            foreach(var folder in folders)
+            {
+                foreach(var filepath in folder)
+                {
+                    Console.WriteLine(filepath);
+                }
+            }
+
+            return folders;
+        }
+
+        public List<InputFile> ReadFiles(List<string[]> folders)
+        {
+            Console.WriteLine("Reading Files");
+            var output = new List<InputFile>();
+            foreach (var folder in folders)
+            {
+                foreach (var filepath in folder)
+                {
+                    var inputFile = new InputFile();
+                    inputFile.Path = filepath;
+  
+                    byte[] contents = File.ReadAllBytes(filepath);
+
+                    var stringBuilder = new StringBuilder();
+                    foreach (var c in contents)
+                    {
+                        stringBuilder.Append((char)c);
+                    }
+
+                    inputFile.Content = stringBuilder.ToString();
+                    output.Add(inputFile);
+                }
+            }
+            return output;
+        }
+    }
+}
