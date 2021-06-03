513c01187347ff508127dd20425f3b9700f0d619
David Ruitter <David.Ruitter@netgo.de>
2021-06-03 11:15:13 +0200

    Update HelpService

--- src/EasyDoc/EasyDoc.Application/Services/HelpService.cs
+++ src/EasyDoc/EasyDoc.Application/Services/HelpService.cs
@@ -7,9 +7,19 @@ namespace EasyDoc.Application.Services
     {
         public void GetHelp()
         {
-            Console.Write("usage: easydoc [--version] [--help]");
-            Console.WriteLine("");
+            Console.WriteLine("usage: easydoc \t[--version | -v] [--help | -h]");
+            Console.WriteLine("\t \t[--input | -i] <absolute input path>");
+            Console.WriteLine("\t \t[--output | -o] <absolute output path>");
+            
             Console.WriteLine("These are common easydoc commands:");
+            Console.WriteLine("");
+            Console.WriteLine("get information about easydoc");
+            Console.WriteLine("  --version | -v \t Returns the version of easydoc you are using");
+            Console.WriteLine("  --help    | -h \t Shows which commands are available");
+            Console.WriteLine("");
+            Console.WriteLine("choose paths");
+            Console.WriteLine("  --input   | -i \t Selects the path which is used to read the files you want to document");
+            Console.WriteLine("  --ouput   | -o \t Selects the path were your file/files are out putted");
             Console.WriteLine("");
         }
     }
