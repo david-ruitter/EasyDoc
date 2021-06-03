1a298223ec658512e7cef9defcb87877d114f0d8
David Ruitter <David.Ruitter@netgo.de>
2021-06-03 11:20:12 +0200

    Only use one WriteLine in HelpService

--- src/EasyDoc/EasyDoc.Application/Services/HelpService.cs
+++ src/EasyDoc/EasyDoc.Application/Services/HelpService.cs
@@ -7,20 +7,18 @@ namespace EasyDoc.Application.Services
     {
         public void GetHelp()
         {
-            Console.WriteLine("usage: easydoc \t[--version | -v] [--help | -h]");
-            Console.WriteLine("\t \t[--input | -i] <absolute input path>");
-            Console.WriteLine("\t \t[--output | -o] <absolute output path>");
-            
-            Console.WriteLine("These are common easydoc commands:");
-            Console.WriteLine("");
-            Console.WriteLine("get information about easydoc");
-            Console.WriteLine("  --version | -v \t Returns the version of easydoc you are using");
-            Console.WriteLine("  --help    | -h \t Shows which commands are available");
-            Console.WriteLine("");
-            Console.WriteLine("choose paths");
-            Console.WriteLine("  --input   | -i \t Selects the path which is used to read the files you want to document");
-            Console.WriteLine("  --ouput   | -o \t Selects the path were your file/files are out putted");
-            Console.WriteLine("");
+            Console.WriteLine(  
+                "usage: easydoc \t[--version | -v] [--help | -h]\n" +
+                "\t \t[--input | -i] <absolute input path>\n" +
+                "\t \t[--output | -o] <absolute output path>\n" +
+                "These are common easydoc commands:\n\n" +
+                "get information about easydoc\n" +
+                "  --version | -v \t Returns the version of easydoc you are using\n" +
+                "  --help    | -h \t Shows which commands are available\n\n" +
+                "choose paths\n" +
+                "  --input   | -i \t Selects the path which is used to read the files you want to document\n" +
+                "  --ouput   | -o \t Selects the path were your file/files are out putted"        
+            );
         }
     }
 }
