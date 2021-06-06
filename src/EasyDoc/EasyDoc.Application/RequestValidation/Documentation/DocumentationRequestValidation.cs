0adff5c099008f5dc7b916a56e543c7002f78046
David Ruitter <David.Ruitter@netgo.de>
2021-06-06 12:11:16 +0200

    Add Documentation Requests

---
+++ src/EasyDoc/EasyDoc.Application/RequestValidation/Documentation/DocumentationRequestValidation.cs
@@ -0,0 +1,31 @@
+﻿using EasyDoc.Application.Requests.Documentation;
+using EasyDoc.Domain.Errors;
+using FluentValidation;
+
+namespace EasyDoc.Application.RequestValidation.Documentation
+{
+    public class DocumentationRequestValidation : AbstractValidator<DocumentationRequest>
+    {
+        public DocumentationRequestValidation()
+        {
+            AddRuleForFileName();
+            AddRuleForFileContent();
+        }
+
+        protected void AddRuleForFileName()
+        {
+            RuleFor(req => req.FileName)
+                .NotEmpty()
+                .WithErrorCode(DomainErrorCodes.InvalidFileName)
+                .WithMessage("Filename may not be empty");
+        }
+
+        protected void AddRuleForFileContent()
+        {
+            RuleFor(req => req.FileContent)
+                .NotEmpty()
+                .WithErrorCode(DomainErrorCodes.InvalidFileContent)
+                .WithMessage("Filecontent may not be empty");
+        }
+    }
+}
---
+++ src/EasyDoc/EasyDoc.Application/Requests/Documentation/DocumentationRequest.cs
@@ -0,0 +1,28 @@
+﻿using EasyDoc.Application.Models;
+using EasyDoc.Application.RequestValidation.Documentation;
+using EasyDoc.Domain.Core.Commands;
+using System;
+
+namespace EasyDoc.Application.Requests.Documentation
+{
+    public class DocumentationRequest : ReturningCommand<CommentOutput>
+    {
+        protected DocumentationRequest(
+            Guid aggregateId,
+            string fileName,
+            string fileContent) : base(aggregateId)
+        {
+            FileName = fileName;
+            FileContent = fileContent;
+        }
+
+        public string FileName { get; set; }
+        public string FileContent { get; set; }
+
+        public override bool IsValid()
+        {
+            ValidationResult = new DocumentationRequestValidation().Validate(this);
+            return ValidationResult.IsValid;
+        }
+    }
+}
