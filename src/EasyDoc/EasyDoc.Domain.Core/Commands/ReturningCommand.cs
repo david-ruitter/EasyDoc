b29d5f515cc32fb5a9f236d19529a0ffa43a5efc
David Ruitter <David.Ruitter@netgo.de>
2021-06-06 12:10:56 +0200

    Add ReturningCommand

---
+++ src/EasyDoc/EasyDoc.Domain.Core/Commands/ReturningCommand.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation.Results;
+using System;
+
+namespace EasyDoc.Domain.Core.Commands
+{
+    public abstract class ReturningCommand<T> : Request<T>
+    {
+        public DateTime Timestamp { get; set; }
+        public ValidationResult ValidationResult { get; set; }
+
+        protected ReturningCommand(Guid aggregateId) : base(aggregateId)
+        {
+        }
+
+        public abstract bool IsValid();
+    }
+}
