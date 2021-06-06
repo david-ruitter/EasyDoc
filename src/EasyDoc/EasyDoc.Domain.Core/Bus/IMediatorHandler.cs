e95486c62eb7c7b63fbdcabb03cac8beab5c9ff8
David Ruitter <David.Ruitter@netgo.de>
2021-06-06 12:24:19 +0200

    Update MediatorHandler

--- src/EasyDoc/EasyDoc.Domain.Core/Bus/IMediatorHandler.cs
+++ src/EasyDoc/EasyDoc.Domain.Core/Bus/IMediatorHandler.cs
@@ -6,6 +6,7 @@ namespace EasyDoc.Domain.Core.Bus
     public interface IMediatorHandler
     {
         Task SendCommandAsync<T>(T command) where T : Command;
-        Task SendRequestAsync<TResponse>(Request<TResponse> request);
+        Task<TResponse> SendRequestAsync<TResponse>(Request<TResponse> request);
+        Task<TResponse> SendReturningCommandAsync<TResponse>(ReturningCommand<TResponse> command);
     }
 }
