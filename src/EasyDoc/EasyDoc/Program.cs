3801ceb00f8b7dabd5f7e9594db2ccf7b61d51ba
David Ruitter <David.Ruitter@netgo.de>
2021-06-07 19:49:41 +0200

    Order some things in the main Program

--- src/EasyDoc/EasyDoc/Program.cs
+++ src/EasyDoc/EasyDoc/Program.cs
@@ -3,15 +3,17 @@ using EasyDoc.Domain.Core.Bus;
 using EasyDoc.Extensions;
 using Microsoft.Extensions.DependencyInjection;
 using System;
-using System.Linq;
 using MediatR;
 using System.Collections.Generic;
+using EasyDoc.Application.Models;
+using System.IO;
+using System.Threading.Tasks;
 
 namespace EasyDoc
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
             var serviceProvider = new ServiceCollection()
                 .AddApplicationServices()
@@ -25,6 +27,10 @@ namespace EasyDoc
 
             var _commandService = serviceProvider.GetService<ICommandService>();
             var result = _commandService.ConvertInputToCommandsAndParams(args);
+            var defaultFormat = "json";
+            var outputPath = Directory.GetCurrentDirectory();
+            var _fileInputService = serviceProvider.GetService<IFileInputService>();
+            var folders = new List<string[]>();
 
             if (result != null)
             {
@@ -40,15 +46,13 @@ namespace EasyDoc
                     if (r.Key == "--version" || r.Key == "-v")
                     {
                         Console.WriteLine("easydoc version: 1.0.0");
+                        return;
                     }
                     if (r.Key == "--output" || r.Key == "-o")
                     {
-                        var _fileService = serviceProvider.GetService<IFileOutputService>();
-                        _fileService.WriteFile();
+                        outputPath = r.Value[0];
+                        continue;
                     }
-
-                    var _fileInputService = serviceProvider.GetService<IFileInputService>();
-                    List<string[]> folders;
                     if (r.Key == "--input" || r.Key == "-i")
                     {
                         folders = _fileInputService.GetFilePaths(r.Value);
@@ -57,14 +61,27 @@ namespace EasyDoc
                     {
                         folders = _fileInputService.GetFilePaths(null);
                     }
-                    var inputFiles = _fileInputService.ReadFiles(folders);
-
-                    var _documentationService = serviceProvider.GetService<IDocumentationService>();
-                    foreach (var inFile in inputFiles)
-                    {
-                        _documentationService.CreateDocumentation(inFile, "json");
-                    }
                 }
+                var inputFiles = _fileInputService.ReadFiles(folders);
+
+                var _documentationService = serviceProvider.GetService<IDocumentationService>();
+                var documentations = new List<CommentOutput>();
+                foreach (var inFile in inputFiles)
+                {
+                    documentations.Add(await _documentationService.CreateDocumentationAsync(inFile));
+                }
+
+                // Format Files
+                var _formatService = serviceProvider.GetService<IFormatService>();
+                var formattedDocumentations = new List<string>();
+                foreach (var doc in documentations)
+                {
+                    formattedDocumentations.Add(_formatService.FormatAs(doc, defaultFormat));
+                }
+
+                var _fileOutputService = serviceProvider.GetService<IFileOutputService>();
+                await _fileOutputService.WriteFile(formattedDocumentations, outputPath);
+                
             }
         }
     }
